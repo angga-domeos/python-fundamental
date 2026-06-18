f73055a34ed833e3acb45b3dcff4eb74fed8e941
ryzuu <oxryzuu@gmail.com>
2026-06-18 21:12:43 +0700

    learn: function

---
+++ condition.py
@@ -0,0 +1,8 @@
+def triangle_calculation():
+    base = int(input("input base: " ))
+    heigh = int(input("input heigh: " ))
+    result =  1/2 * base * heigh
+    print(f"triagle calculation: {result}")
+
+triangle_calculation()
+
