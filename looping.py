43138bf126c4f7fd7bc8d76af17866247571c8a0
ryzuu <oxryzuu@gmail.com>
2026-06-19 14:31:04 +0700

    learn: looping for

--- looping.py
+++ looping.py
@@ -1,6 +1,29 @@
 # looping 1
 # looping with while use conditional
 a = 1
-while a < 10:
-    print(f"looping: {a}")
-    a += 1
+# while a < 10:
+#     print(f"looping: {a}")
+#     a += 1
+
+# looping for with iterations
+# for i in range(1,10):
+#     print(i)
+
+# looping dictionary
+student = {
+    "name": "linux",
+    "age": 23,
+    "address": "america"
+}
+
+# looping dictionary take values
+# for i in student.values():
+#     print(i)
+
+# looping dictionary take key
+# for i in student:
+#     print(i)
+
+# looping dictionary take key and value
+for k, v in student.items():
+    print(f"key {k}, value {v}")
