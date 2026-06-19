71058752fa3d104f8f75949730268d9674569a38
ryzuu <oxryzuu@gmail.com>
2026-06-19 14:54:31 +0700

    learn: collections(list)

---
+++ collections.py
@@ -0,0 +1,38 @@
+# list
+# day = ["sunday", "monday", "thurday", "wednesday", "thursday", "friday", "saturday"]
+
+# print all list
+# print(day)
+
+# print one list
+# print(day[0])
+
+# list and condition
+# input_day = input("enter day: " )
+# if input_day in day:
+#     print("today is: " + input_day)
+# else:
+#     print("day not define")
+#     for i in day:
+#         print("day: " + str(day))
+#         break
+
+
+# add and remove list
+number = [1, 2, 3, 4, 5]
+
+# menambahkan data di bagian paling akhir list
+# number.append(6)
+# print(number)
+
+# menambahkan nilai pada index tertentu pada list
+# number.insert(0, 0)
+# print(number)
+
+# menghapus nilai list berdasarkan nama itemnya
+# number.remove(5)
+# print(number)
+
+# menghapus index tertentu dan jika index kosong maka default menghapus di bagian paling akhir list
+number.pop(0)
+print(number)
